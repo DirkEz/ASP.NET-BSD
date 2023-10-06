cd0ea778cdac00c209d393106e3b43360ca4b9af
DirkEz <dirk.schaafstra@gmail.com>
2023-10-06 13:21:17 +0200

    .

--- ASP.NET GEOPATH BSD/ASP.NET GEOPATH BSD/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASP.NET_GEOPATH_BSD.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
