e795451bc8c014883fc0205dd69f1bce35dfd701
bjoreno <bjodegens@hotmail.com>
2023-10-27 13:52:47 +0200

    added crud movie

---
+++ geoprofs/geoprofs/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using geoprofs.Data;
+using geoprofs.Models;
+
+namespace geoprofs.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly geoprofs.Data.geoprofsContext _context;
+
+        public DetailsModel(geoprofs.Data.geoprofsContext context)
+        {
+            _context = context;
+        }
+
+      public Movie Movie { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Movie == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movie.FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Movie = movie;
+            }
+            return Page();
+        }
+    }
+}
